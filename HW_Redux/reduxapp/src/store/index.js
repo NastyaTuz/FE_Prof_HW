379444b43b09913fcc83492b29ab628c7d1605cf
NastyaTuz <62496991+NastyaTuz@users.noreply.github.com>
2024-01-18 18:27:24 +0100

    Delete HW_Redux/reduxapp directory

--- HW_Redux/reduxapp/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import { productReducer } from "./productReducer";
-
-const rootReducer = combineReducers({
-  products: productReducer,
-});
-
-export const store = createStore(rootReducer);
